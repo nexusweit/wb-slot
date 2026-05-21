7822d472801561ea857d1d99bf9b17f8f90fadbb
nexusweit <162042025+nexusweit@users.noreply.github.com>
2026-05-21 23:02:34 +0300

    Delete api directory

--- api/index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  // Берем ссылку из переменной REDIRECT_URL или шлем на google, если забыли настроить
-  const target = process.env.REDIRECT_URL || 'https://google.com';
-  
-  // Выполняем редирект (307 - временный, чтобы браузер не кэшировал его навсегда)
-  res.redirect(307, target);
-}
